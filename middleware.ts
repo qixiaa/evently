b18d6d8f870c26de44c1483ef15c2e404a4a91f1
qixiaa <aqixiaa@gmail.com>
2024-07-08 20:33:26 +0800

    first commit

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// const isPublicRoute = createRouteMatcher([
+//   "/",
+//   "/events/:id",
+//   "/api/webhook/clerk",
+//   "/api/webhook/stripe",
+//   "/api/uploadthing",
+// ]);
+
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/forum(.*)"]);
+
+export default clerkMiddleware((auth, request) => {
+  // if (!isPublicRoute(request)) {
+  //   auth().protect();
+  // }
+  // if (isProtectedRoute(request)) auth().protect();
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
